a59e3a523c50a861306b15256698305d75d26936
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-25 21:20:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -78,7 +78,7 @@ def movies_with_directors_set(source)
       movies_array << movie_with_director_name(name, movies_collection)
       movies_index += 1
     end
-    movies_with_directors_name << movies_array
+    movies_with_directors_name << single_director_movies_array
     row_index += 1
   end
   return final_array
